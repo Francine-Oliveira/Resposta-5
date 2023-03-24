c7a57e561652da00e15b102d0bf82fee8454d903
Francine-Oliveira <olive.fran86@gmail.com>
2023-03-23 23:29:50 -0300

    first commit

--- Ex5-CharInvertidos/Program.cs
+++ Ex5-CharInvertidos/Program.cs
@@ -3,7 +3,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string invertida = string.Empty;
+        int i;
+
+        Console.Write("Digite uma palavra: ");
+        string palavra = Console.ReadLine();
+
+        for (i = palavra.Length - 1; i >= 0; i--)
+        {
+            invertida += palavra[i];
+        }
+
+        Console.WriteLine("{0} ao contrário é {1}. ", palavra, invertida);
+
+        Console.ReadKey();
     }
 }
 
